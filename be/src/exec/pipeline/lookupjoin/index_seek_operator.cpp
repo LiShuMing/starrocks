0d1aed951cc95a8834c5e8672d44c8aa628fb993
Kevin Li <ming.moriarty@gmail.com>
2022-09-19 18:55:49 +0800

    fix bugs

--- be/src/exec/pipeline/lookupjoin/index_seek_operator.cpp
+++ be/src/exec/pipeline/lookupjoin/index_seek_operator.cpp
@@ -6,17 +6,25 @@
 #include "exec/pipeline/operator.h"
 #include "exec/vectorized/olap_scan_node.h"
 #include "util/runtime_profile.h"
+#include "storage/chunk_helper.h"
+#include "storage/predicate_parser.h"
 
 namespace starrocks::pipeline {
 
 IndexSeekOperator::IndexSeekOperator(OperatorFactory* factory, int32_t id, int32_t plan_node_id, const int32_t driver_sequence,
-                                     const std::vector<std::string>& key_column_names)
+                                     TInternalScanRange* scan_range, const std::vector<std::string>& key_column_names,
+                                     const TupleDescriptor* tuple_desc, const std::vector<ExprContext*>& conjunct_ctxs)
         : Operator(factory, id, "nestloop_join_build", plan_node_id, driver_sequence),
-          _key_column_names(key_column_names){
+          _scan_range(scan_range), _key_column_names(key_column_names), _tuple_desc(tuple_desc),
+          _conjunct_ctxs(conjunct_ctxs) {
+}
+
+IndexSeekOperator::~IndexSeekOperator() {
+    _reader.reset();
+    _predicate_free_pool.clear();
 }
 
 Status IndexSeekOperator::prepare(RuntimeState* state) {
-    _slots = &tuple_desc->slots();
     RETURN_IF_ERROR(_init_tablet_reader(state));
     return Status::OK();
 }
@@ -45,19 +53,32 @@ Status IndexSeekOperator::_init_conjuncts_manager(RuntimeState* state) {
 }
 
 Status IndexSeekOperator::_init_reader_params(RuntimeState* state,
-                                              const std::vector<std::unique_ptr<OlapScanRange>>& key_ranges,
-                                              const std::vector<uint32_t>& scanner_columns,
-                                              std::vector<uint32_t>& reader_columns) {
+                                              const std::vector<std::unique_ptr<OlapScanRange>>& key_ranges) {
     _params.is_pipeline = true;
     _params.chunk_size = state->chunk_size();
     _params.reader_type = READER_QUERY;
     _params.skip_aggregation = true;
     _params.runtime_state = state;
     _params.use_page_cache = !config::disable_storage_page_cache;
-    _morsel->init_tablet_reader_params(&_params);
+
+    PredicateParser parser(_tablet->tablet_schema());
+    std::vector<PredicatePtr> preds;
+    RETURN_IF_ERROR(_conjuncts_manager.get_column_predicates(&parser, &preds));
+    for (auto& p : preds) {
+        if (parser.can_pushdown(p.get())) {
+            _params.predicates.push_back(p.get());
+        } else {
+            _not_push_down_predicates.add(p.get());
+        }
+        _predicate_free_pool.emplace_back(std::move(p));
+    }
 
     // Range
-    if (key_range->begin_scan_range.get_value(0) != NEGATIVE_INFINITY) {
+    for (const auto& key_range : key_ranges) {
+        if (key_range->begin_scan_range.size() == 1 && key_range->begin_scan_range.get_value(0) == NEGATIVE_INFINITY) {
+            continue;
+        }
+
         _params.range = key_range->begin_include ? TabletReaderParams::RangeStartOperation::GE
                                                  : TabletReaderParams::RangeStartOperation::GT;
         _params.end_range = key_range->end_include ? TabletReaderParams::RangeEndOperation::LE
@@ -66,11 +87,13 @@ Status IndexSeekOperator::_init_reader_params(RuntimeState* state,
         _params.start_key.push_back(key_range->begin_scan_range);
         _params.end_key.push_back(key_range->end_scan_range);
     }
+
     return Status::OK();
 }
 
 Status IndexSeekOperator::_init_scanner_columns(std::vector<uint32_t>& scanner_columns) {
-    for (auto slot : *_slots) {
+    auto slots = _tuple_desc->slots();
+    for (auto slot : slots) {
         DCHECK(slot->is_materialized());
         int32_t index = _tablet->field_index(slot->col_name());
         if (index < 0) {
@@ -91,15 +114,17 @@ Status IndexSeekOperator::_init_tablet_reader(RuntimeState* state) {
     // init tablet
     // TODO: how to define scan_range?
     RETURN_IF_ERROR(_get_tablet(_scan_range));
-    // init output columns
-    std::vector<uint32_t> reader_columns;
-    RETURN_IF_ERROR(_init_scanner_columns(reader_columns));
+
+    // output columns of `this` OlapScanner, i.e, the final output columns of `get_chunk`.
+    std::vector<uint32_t> scanner_columns;
+
+    RETURN_IF_ERROR(_init_scanner_columns(scanner_columns));
     // _init_reader_params
-    RETURN_IF_ERROR(_init_reader_params(_params));
+    RETURN_IF_ERROR(_init_reader_params(state, _key_ranges));
 
     const TabletSchema& tablet_schema = _tablet->tablet_schema();
     starrocks::vectorized::Schema child_schema =
-            ChunkHelper::convert_schema_to_format_v2(tablet_schema, reader_columns);
+            ChunkHelper::convert_schema_to_format_v2(tablet_schema, scanner_columns);
 
     std::vector<std::unique_ptr<OlapScanRange>> key_ranges;
     RETURN_IF_ERROR(_conjuncts_manager.get_key_ranges(&key_ranges));
@@ -117,6 +142,14 @@ void IndexSeekOperator::close(RuntimeState* state) {
 }
 
 StatusOr<vectorized::ChunkPtr> IndexSeekOperator::pull_chunk(RuntimeState* state) {
+    if (state->is_cancelled()) {
+        return Status::Cancelled("canceled state");
+    }
+    _cur_chunk.reset(ChunkHelper::new_chunk_pooled(_prj_iter->output_schema(), state->chunk_size(), true));
+    do {
+        RETURN_IF_ERROR(_prj_iter->get_next(_cur_chunk.get()));
+        // TODO: remove unused predicate.
+    } while (_cur_chunk->num_rows() == 0);
     return std::move(_cur_chunk);
 }
 
@@ -126,7 +159,6 @@ Status IndexSeekOperator::set_finishing(RuntimeState* state) {
 }
 
 Status IndexSeekOperator::push_chunk(RuntimeState* state, const vectorized::ChunkPtr& chunk) {
-    RETURN_IF_ERROR(_prj_iter->get_next(chunk));
     return Status::OK();
 }
 
--- be/src/exec/pipeline/lookupjoin/index_seek_operator.h
+++ be/src/exec/pipeline/lookupjoin/index_seek_operator.h
@@ -7,19 +7,21 @@
 #include "column/vectorized_fwd.h"
 #include "exec/pipeline/nljoin/nljoin_context.h"
 #include "exec/pipeline/operator.h"
+#include "exec/vectorized/olap_scan_prepare.h"
 #include "storage/rowset/segment_options.h"
 #include "storage/tablet_reader.h"
 
 namespace starrocks::pipeline {
+using namespace vectorized;
 
 // IndexSeekOperator
 class IndexSeekOperator final : public Operator {
 public:
     IndexSeekOperator(OperatorFactory* factory, int32_t id, int32_t plan_node_id, const int32_t driver_sequence,
-                      const std::vector<std::string>& key_column_names,                           const std::vector<std::string>& key_column_names, );
-);
+                      TInternalScanRange* scan_range, const std::vector<std::string>& key_column_names,
+                      const TupleDescriptor* tuple_desc, const std::vector<ExprContext*>& conjunct_ctxs);
 
-    ~IndexSeekOperator() override = default;
+    ~IndexSeekOperator() override;
 
     Status prepare(RuntimeState* state) override;
 
@@ -39,36 +41,48 @@ public:
 
 private:
     Status _get_tablet(const TInternalScanRange* scan_range);
-    Status _init_conjuncts_manager();
+    Status _init_conjuncts_manager(RuntimeState* state);
     Status _init_scanner_columns(std::vector<uint32_t>& scanner_columns);
+    Status _init_reader_params(RuntimeState* state,
+                               const std::vector<std::unique_ptr<OlapScanRange>>& key_ranges);
     Status _init_tablet_reader(RuntimeState* state) ;
 
     bool _is_finished = false;
     vectorized::ChunkPtr _cur_chunk = nullptr;
 
+    TabletSharedPtr _tablet;
+    TInternalScanRange* _scan_range;
     const std::vector<std::string>& _key_column_names;
     const TupleDescriptor* _tuple_desc;
-    const std::vector<SlotDescriptor*>* _slots = nullptr;
+    const std::vector<ExprContext*>& _conjunct_ctxs;
 
     int64_t _version;
-    TInternalScanRange* _scan_range;
-    std::vector<std::unique_ptr<TInternalScanRange>> _scan_ranges;
-    OlapScanConjunctsManager _conjuncts_manager;
-    std::vector<ExprContext*> _not_push_down_conjuncts;
+    ObjectPool _obj_pool;
     vectorized::TabletReaderParams _params{};
+    vectorized::OlapScanConjunctsManager _conjuncts_manager;
+    std::vector<std::unique_ptr<OlapScanRange>> _key_ranges;
+    vectorized::ConjunctivePredicates _not_push_down_predicates;
+    std::vector<ExprContext*>* _not_push_down_conjuncts;
+
+    using PredicatePtr = std::unique_ptr<vectorized::ColumnPredicate>;
+    std::vector<PredicatePtr> _predicate_free_pool;
+
     // NOTE: _reader may reference the _predicate_free_pool, it should be released before the _predicate_free_pool
     std::shared_ptr<vectorized::TabletReader> _reader;
+    // projection iterator, doing the job of choosing |_scanner_columns| from |_reader_columns|.
+    std::shared_ptr<vectorized::ChunkIterator> _prj_iter;
 };
 
 class IndexSeekOperatorFactory final : public OperatorFactory {
 public:
     IndexSeekOperatorFactory(int32_t id, int32_t plan_node_id)
-            : OperatorFactory(id, "lookupjoin_build", plan_node_id) {}
+            : OperatorFactory(id, "index_seek_operator", plan_node_id) {}
 
     ~IndexSeekOperatorFactory() override = default;
 
     OperatorPtr create(int32_t degree_of_parallelism, int32_t driver_sequence) override {
-        return std::make_shared<IndexSeekOperator>(this, _id, _plan_node_id, driver_sequence);
+//        return std::make_shared<IndexSeekOperator>(this, _id, _plan_node_id, driver_sequence);
+        return nullptr;
     }
 
 private:

a8b51dedcd7f5ee7cc685ac586fbf7bbae8d93cc
shuming.li <ming.moriarty@gmail.com>
2025-03-13 15:15:24 +0800

    fix bugs
    Signed-off-by: shuming.li <ming.moriarty@gmail.com>

---
+++ be/src/exprs/agg/data_sketch/ds_theta.h
@@ -0,0 +1,184 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#pragma once
+
+#include <memory>
+#include <string>
+
+#include "runtime/memory/counting_allocator.h"
+#include "runtime/memory/mem_chunk.h"
+#include "runtime/memory/mem_chunk_allocator.h"
+#include "util/slice.h"
+
+#include "datasketches/theta_sketch.hpp"
+#include "datasketches/theta_union.hpp"
+
+namespace starrocks {
+
+class DataSketchesTheta {
+public:
+    using alloc_type = STLCountingAllocator<uint64_t>;
+    using theta_sketch_type = datasketches::update_theta_sketch_alloc<alloc_type>;
+    using theta_union_type = datasketches::theta_union_alloc<alloc_type>;
+    using theta_wrapped_type = datasketches::wrapped_compact_theta_sketch_alloc<alloc_type>;
+    using sketch_data_alloc_type = typename std::allocator_traits<alloc_type>::template rebind_alloc<uint8_t>;
+    using sketch_data_type = std::vector<uint8_t, sketch_data_alloc_type>;
+
+    explicit DataSketchesTheta(int64_t* memory_usage) : _memory_usage(memory_usage) {
+        _sketch = std::make_unique<theta_sketch_type>(theta_sketch_type::builder(alloc_type(_memory_usage)).build());
+    }
+
+    DataSketchesTheta(const DataSketchesTheta& other) = delete;
+    DataSketchesTheta& operator=(const DataSketchesTheta& other) = delete;
+
+    DataSketchesTheta(DataSketchesTheta&& other) noexcept
+            : _memory_usage(std::move(other._memory_usage)), _sketch(std::move(other._sketch)) {
+        if (other._sketch_union != nullptr) {
+            this->_sketch_union = std::move(other._sketch_union);
+        }
+    }
+
+    DataSketchesTheta& operator=(DataSketchesTheta&& other) noexcept {
+        if (this != &other) {
+            this->_memory_usage = std::move(other._memory_usage);
+            this->_sketch = std::move(other._sketch);
+            if (other._sketch_union != nullptr) {
+                this->_sketch_union = std::move(other._sketch_union);
+            }
+        }
+        return *this;
+    }
+
+    explicit DataSketchesTheta(const Slice& src, int64_t* memory_usage) : _memory_usage(memory_usage) {
+        if (!deserialize(src)) {
+            LOG(WARNING) << "Failed to init DataSketchesFrequent from slice, will be reset to 0.";
+        }
+    }
+
+    ~DataSketchesTheta() = default;
+
+    void update(uint64_t hash_value) {
+        _sketch->update(hash_value);
+        _is_changed = true;
+    }
+
+    void merge(const DataSketchesTheta& other) {
+        if (_sketch_union == nullptr) {
+            _sketch_union =
+                    std::make_unique<theta_union_type>(theta_union_type::builder(alloc_type(_memory_usage)).build());
+        }
+        _sketch_union->update(other._sketch->compact());
+        if (other._sketch_union != nullptr) {
+            _sketch_union->update(other._sketch_union->get_result());
+        }
+        _is_changed = true;
+    }
+
+    int64_t mem_usage() const { return _memory_usage == nullptr ? 0L : *_memory_usage; }
+
+    size_t serialize(uint8_t* dst) const {
+        serialize_if_needed();
+        std::copy(_sketch_data->begin(), _sketch_data->end(), dst);
+        return _sketch_data->size();
+    }
+
+    uint64_t serialize_size() const {
+        serialize_if_needed();
+        return _sketch_data->size();
+    }
+
+    void serialize_if_needed() const {
+        if (UNLIKELY(_sketch == nullptr)) {
+            _sketch =
+                    std::make_unique<theta_sketch_type>(theta_sketch_type::builder(alloc_type(_memory_usage)).build());
+        }
+        if (_is_changed) {
+            auto resultTheta_union = theta_union_type(theta_union_type::builder(alloc_type(_memory_usage)).build());
+            resultTheta_union.update(_sketch->compact());
+            if (_sketch_union != nullptr) {
+                resultTheta_union.update(_sketch_union->get_result());
+            }
+            auto sketch_ser = resultTheta_union.get_result().serialize();
+            _sketch_data = std::make_unique<sketch_data_type>(
+                    sketch_data_type(sketch_ser.begin(), sketch_ser.end(), sketch_ser.get_allocator()));
+            _is_changed = false;
+        }
+    }
+
+    bool deserialize(const Slice& slice) {
+        if (!is_valid(slice)) {
+            return false;
+        }
+        DCHECK(_sketch == nullptr);
+        _sketch = std::make_unique<theta_sketch_type>(theta_sketch_type::builder(alloc_type(_memory_usage)).build());
+        try {
+            auto sketch_warp = theta_wrapped_type::wrap((uint8_t*)slice.data, slice.size);
+            if (_sketch_union == nullptr) {
+                _sketch_union = std::make_unique<theta_union_type>(
+                        theta_union_type::builder(alloc_type(_memory_usage)).build());
+            }
+            _sketch_union->update(sketch_warp);
+        } catch (std::logic_error& e) {
+            LOG(WARNING) << "DataSketchesTheta deserialize error: " << e.what();
+            return false;
+        }
+        return true;
+    }
+
+    static bool is_valid(const Slice& slice) {
+        if (slice.size < 1) {
+            return false;
+        }
+        return true;
+    }
+
+    int64_t estimate_cardinality() const {
+        if (_sketch == nullptr && _sketch_union == nullptr) {
+            return 0;
+        }
+        if (_sketch_union == nullptr) {
+            return _sketch->get_estimate();
+        } else {
+            auto resultTheta_union = theta_union_type(theta_union_type::builder(alloc_type(_memory_usage)).build());
+            resultTheta_union.update(_sketch_union->get_result());
+            if (_sketch != nullptr) {
+                resultTheta_union.update(_sketch->compact());
+            }
+            return resultTheta_union.get_result().get_estimate();
+        }
+    }
+
+    void clear() {
+        if (_sketch != nullptr) {
+            _sketch->reset();
+        }
+
+        if (_sketch_union != nullptr) {
+            _sketch_union.reset();
+        }
+    }
+
+private:
+    int64_t* _memory_usage;
+    mutable std::unique_ptr<theta_sketch_type> _sketch = nullptr;
+    mutable std::unique_ptr<theta_union_type> _sketch_union = nullptr;
+    mutable std::unique_ptr<sketch_data_type> _sketch_data = nullptr;
+    mutable bool _is_changed = true;
+};
+
+} // namespace starrocks
